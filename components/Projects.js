b101478519ae438d28d1a4455979bcf219cfca5d
Mark Hou <myhou0720@gmail.com>
2026-01-27 16:15:38 -0800

    setup component for projects, added boilerplate

---
+++ components/Projects.js
@@ -0,0 +1,11 @@
+export default function Projects() {
+    return (
+        <section id="projects" className="py-20 px-6 flex item-center justify-center">
+            <div className="max-w-4xl mx-auto">
+                <h2 className="text-2xl md:text-3xl font-bold">
+                    More coming soon...
+                </h2>
+            </div>
+        </section>
+    )
+}
